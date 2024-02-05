fc79739833791b607cccb53f3638ffa2e7c47384
Andrew Cookson <andrewcookson@email.com>
2024-02-05 18:27:59 +0000

    enhanced: file structure and script code

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use("/example", express.static("example"));
+app.use("/awesome", express.static("awesome"));
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
