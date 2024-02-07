1032d9629f3e67ab8512714d7ebb93e7f1d4aba7
Andrew Cookson <andrewcookson@email.com>
2024-02-07 22:59:33 +0000

    enchancement: added PUT function and better responces

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 const connection = async () => {
   await mongoose.connect(process.env.MONGO_URI);
-  console.log("DB connection is working");
+  console.log("Connected to MongoDB");
 };
 
 connection();
@@ -37,9 +37,14 @@ const Book = mongoose.model("Book", bookSchema);
 app.get("/books", async (request, response) => {
   try {
     const books = await Book.find();
-    return response.status(200).json({ data: books });
+    return response.status(200).json({
+      data: books,
+      message: "All books have been successfully retrieved.",
+    });
   } catch (error) {
-    return response.status(400).json(error);
+    return response
+      .status(500)
+      .json({ message: "Error occurred while retrieving books." });
   }
 });
 
@@ -47,36 +52,73 @@ app.get("/books", async (request, response) => {
 app.get("/books/getfirstbook", async (request, response) => {
   try {
     const books = await Book.find();
-    return response.status(200).json({ data: books[0] });
+    const firstBook = books[0];
+
+    if (!firstBook) {
+      return response.status(404).json({ message: "Book not found." });
+    }
+
+    return response.status(200).json({
+      data: firstBook,
+      message: "First book retrieved successfully.",
+    });
   } catch (error) {
-    return response.status(400).json(error);
+    return response
+      .status(500)
+      .json({ message: "An error occurred while retrieving the first book." });
   }
 });
 
 // Handle POST request to add a new book
 app.post("/books", async (request, response) => {
   try {
-    const newBook = await Book.create(request.body);
-    return response.status(201).json({ data: newBook });
+    await Book.create(request.body);
+    return response.status(201).json({ message: "Book added successfully." });
   } catch (error) {
-    return response.status(400).json(error);
+    const errorMessage = "Error occurred while creating a new book.";
+    return response.status(400).json({ message: errorMessage });
   }
 });
 
 // Handle PUT request to update a book's author
-app.put("/books", async (request, response) => {});
+app.put("/books", async (request, response) => {
+  try {
+    const book = await Book.findOne({ title: request.body.title });
+
+    if (!book) {
+      return response.status(404).json({ message: "Book could not be found." });
+    }
+
+    book.author = request.body.author;
+
+    await book.save();
+
+    return response
+      .status(200)
+      .json({ message: "Author updated successfully." });
+  } catch (error) {
+    return response.status(500).json({ message: "Error updating author." });
+  }
+});
 
 // Handle DELETE request to delete a book by title
 app.delete("/books", async (request, response) => {
   try {
     const deletedBook = await Book.findOneAndDelete(request.body);
-    return response.status(200).json({ data: deletedBook });
+
+    if (!deletedBook) {
+      return response.status(404).json({ message: "Book could not be found." });
+    }
+
+    return response.status(200).json({ message: "Book deleted successfully." });
   } catch (error) {
-    return response.status(400).json(error);
+    return response
+      .status(500)
+      .json({ message: "Error occurred while deleting the book." });
   }
 });
 
 // Start the server and listen on port 5001
 app.listen(5001, () => {
-  console.log("server is listening (port 5001)");
+  console.log("Listening on port 5001");
 });
